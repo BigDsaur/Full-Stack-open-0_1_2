1b8eb05f3d0a65e638553d89b68c9c31fb7ac14a
BigDsaur <Joona879@gmail.com>
2025-04-02 13:12:53 +0300

    1.1 - 1.2 tehtävät tehty

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,49 @@
+const Header = ({ course }) => {
+  console.log("Rendering Header");
+  return <h1>{course}</h1>;
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  console.log("Rendering Content");
+  return (
+    <div>
+      <Part {...parts[0]} />
+      <Part {...parts[1]} />
+      <Part {...parts[2]} />
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  console.log("Rendering Total");
+  return (
+    <p>Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
+  );
+};
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = 'Half Stack application development';
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 },
+  ];
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
