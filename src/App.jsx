477be48e47424f938563508ec4bbed36085b1357
BigDsaur <Joona879@gmail.com>
2025-07-21 17:14:57 +0300

    Anekdootti 1.12

--- src/App.jsx
+++ src/App.jsx
@@ -6,51 +6,29 @@ const Button = (props) => (
   </button>
 )
 
-const StatisticLine = ({ text, value }) => (
-  <tr>
-    <td>{text}</td>
-    <td>{value}</td>
-  </tr>
-)
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
 
-const Statistics = ({ good, neutral, bad }) => {
-  const total = good + neutral + bad
-  const average = total === 0 ? 0 : (good - bad) / total
-  const positive = total === 0 ? 0 : (good / total) * 100
+  const [selected, setSelected] = useState(0)
 
-  if (total === 0) {
-    return <p>No feedback given</p>
+  const handleNextAnecdote = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomIndex)
   }
 
   return (
-    <table>
-      <tbody>
-        <StatisticLine text="Good" value={good} />
-        <StatisticLine text="Neutral" value={neutral} />
-        <StatisticLine text="Bad" value={bad} />
-        <StatisticLine text="All" value={total} />
-        <StatisticLine text="Average" value={average.toFixed(1)} />
-        <StatisticLine text="Positive" value={positive.toFixed(1) + ' %'} />
-      </tbody>
-    </table>
-  )
-}
-
-const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  return (
     <div>
-      <h1> GIVE FEEDBACK </h1>
-      <Button onClick={() => setGood(good + 1)} text="Good" />
-      <Button onClick={() => setNeutral(neutral + 1)} text="Neutral" />
-      <Button onClick={() => setBad(bad + 1)} text="Bad"/>
-      <h1> Stats </h1>
-
-      <Statistics good={good} neutral={neutral} bad={bad} />
-
+      <p>{anecdotes[selected]}</p>
+      <Button onClick={handleNextAnecdote} text='Next Anecdote'/>
     </div>
   )
 }
