44665d391b18097f3b2fbe58394342f229b13241
BigDsaur <Joona879@gmail.com>
2025-07-21 15:45:54 +0300

    Unicafe 1.6

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,60 @@
+//import { useState } from 'react'
+
+//const Display = props => <div>{props.value}</div>
+
+//const Button = (props) => (
+//  <button onClick={props.onClick}>
+//    {props.text}
+//  </button>
+//)
+//
+//const App = () => {
+//  const [value, setValue] = useState(10)
+
+//  const setToValue = newValue => {
+//    console.log('value now', newValue)
+//    setValue(newValue)
+//  }
+
+//  return (
+//    <div>
+//      <Display value={value} />
+//      <Button onClick={() => setToValue(1000)} text="thousand" />
+//      <Button onClick={() => setToValue(0)} text="reset" />
+//      <Button onClick={() => setToValue(value + 1)} text="increment" />
+//    </div>
+//  )
+//}
+
+//export default App 
+
+
 import { useState } from 'react'
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
-
-const Button = (props) => { 
-  console.log('props value is', props)
-  const { onClick, text } = props
-  return (
-    <button onClick={onClick}>
-      {text}
-    </button>
-    )
-  }
+const Button = (props) => (
+  <button onClick={props.onClick}>
+    {props.text}
+  </button>
+)
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-  }
-
-debugger
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      {left}
-
-      <Button onClick={handleLeftClick} text='left' />
-      <Button onClick={handleRightClick} text='right' />
-      {right}
-      <History allClicks={allClicks} />
+      <h1> GIVE FEEDBACK </h1>
+      <Button onClick={() => setGood(good + 1)} text="Good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="Neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="Bad"/>
+      <h1> Stats </h1>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
     </div>
   )
 }
-export default App 
+
+export default App
