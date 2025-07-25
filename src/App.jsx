2570501014836c710bfd690bf23ad1712b59d7d1
BigDsaur <Joona879@gmail.com>
2025-07-25 12:51:11 +0300

    Jatkoa Refaktorointia - moduulit

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,13 @@
-import { useState } from 'react'
+const Note = ({ note }) => {
+  return (<li>{note.content}</li>)}
 
-const Button = (props) => (
-  <button onClick={props.onClick}>
-    {props.text}
-  </button>
-)
-
-const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-
-  const [selected, setSelected] = useState(0)
-  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
-
-  const handleNextAnecdote = () => {
-  if (anecdotes.length <= 1) return
-
-  let randomIndex
-  do {
-    randomIndex = Math.floor(Math.random() * anecdotes.length)
-  } while (randomIndex === selected)
-  setSelected(randomIndex)
-}
-
-  const handleVote = () => {
-    const newVotes = [...votes]
-    newVotes[selected] += 1
-    setVotes(newVotes)
-  }
-
-  const maxVotes = Math.max(...votes)
-  const mostVotedIndex = votes.indexOf(maxVotes)
-
+const App = ({ notes }) => {
   return (
     <div>
-      <p>{anecdotes[selected]}</p>
-      <p>has {votes[selected]} votes</p>
-      <Button onClick={handleVote} text='Vote'/>
-      <Button onClick={handleNextAnecdote} text='Next Anecdote'/>
-      <h1>Most voted Anecdote</h1>
-      {(() => {
-        if (maxVotes > 0) {
-          return (
-            <div>
-              <p>{anecdotes[mostVotedIndex]}</p>
-              <p>{maxVotes} votes</p>
-            </div>
-          )
-        } else {
-          return <p>No votes yet</p>
-        }
-})()}
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => <Note key={note.id} note={note} />)}
+      </ul>
     </div>
   )
 }
