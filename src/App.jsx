15d9f10e34288ba28e76d077bda5e843d3bd9779
BigDsaur <Joona879@gmail.com>
2025-07-28 15:37:17 +0300

    puhelinluettelo 2.8

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '123-456-789' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
     
   const addName = (event) => {
     event.preventDefault()
@@ -12,22 +13,36 @@ const App = () => {
     const nameExists = persons.some(person => person.name === newName)
 
     if (nameExists) {
-    alert(`${newName} is already added to the phonebook`)
-    return
+      alert(`${newName} is already added to the phonebook`)
+      return
   }
 
-    const nameObject  = { name: newName }
+    if (newName === '') {
+      alert(`you cannot add an empty name`)
+      return
+    }
+
+    const nameObject  = {
+      name: newName,
+      number: newNumber
+    }
+    
     setPersons(persons.concat(nameObject))
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   const logPersons = () => {
     persons.forEach((person, index) => {
-      console.log(index, person.name)
+      console.log(index, person.name, person.number)
     })
   }
 
@@ -35,9 +50,8 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
-        <div>
-          name:  <input value={newName} onChange={handleNameChange} />
-        </div>
+        <div> name:  <input value={newName} onChange={handleNameChange} /> </div>
+        <div> number: <input value={newNumber} onChange={handleNumberChange}/></div>
         <div>
           <button type="submit">add</button>
         </div>
@@ -48,10 +62,9 @@ const App = () => {
       <h2>Names</h2>
       <ul>
         {persons.map((person, index) => {
-          return <li key={index}>{person.name}</li>
+          return <li key={index}>{person.name} {person.number}</li>
         })}
       </ul>
-      <div>debug: {newName}</div>
     </div>
   )
 }
