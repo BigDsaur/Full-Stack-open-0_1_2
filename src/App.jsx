05b435bd70cecb272c7d3bbd3882ca98037ba4f6
BigDsaur <Joona879@gmail.com>
2025-07-28 14:27:39 +0300

    puhelinluettelo 2.6

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,52 @@
 import { useState } from 'react'
-import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('a new note...') 
-  const [showAll, setShowAll] = useState(true)
-
-  const addNote = (event) => {
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+    
+  const addName = (event) => {
     event.preventDefault()
     console.log('button clicked', event.target)
-    const noteObject = {
-      content: newNote,
-      important: Math.random() > 0.5,
-      id: String(notes.length + 1),
-    }
-
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    const nameObject  = { name: newName }
+    setPersons(persons.concat(nameObject))
+    setNewName('')
   }
 
-  const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
   }
 
-  const notesToShow = showAll? notes: notes.filter(note => note.important)
-  
+  const logPersons = () => {
+    persons.forEach((person, index) => {
+      console.log(index, person.name)
+    })
+  }
+
   return (
     <div>
-      <h1>Notes</h1>
-      <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
-        </button>
-      </div>  
-      <ul>
-        {notesToShow.map(note =>
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
-        <button type="submit">save</button>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name:  <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
       </form>
+
+      <button onClick={logPersons}>Console logi</button>
+
+      <h2>Names</h2>
+      <ul>
+        {persons.map((person, index) => {
+          return <li key={index}>{person.name}</li>
+        })}
+      </ul>
+      <div>debug: {newName}</div>
     </div>
   )
 }
 
-export default App 
+export default App
