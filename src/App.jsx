331ceacef3c2d7df063f842c3bd3f3432c846718
BigDsaur <Joona879@gmail.com>
2025-07-28 15:39:31 +0300

    Tauko

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '123-456-789' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
     
