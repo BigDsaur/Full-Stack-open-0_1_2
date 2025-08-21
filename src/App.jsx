b43a22045c080d53380a8abd164eca3176f1e529
BigDsaur <Joona879@gmail.com>
2025-08-22 00:05:35 +0300

    jatkoa

--- src/App.jsx
+++ src/App.jsx
@@ -2,56 +2,48 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
 
-const App = (props) => {
+const App = () => {
   const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('') 
+  const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
-      .catch(error => {
-        console.error('Error fetching notes:', error)
-      })
+    axios.get('http://localhost:3001/notes').then((response) => {
+      setNotes(response.data)
+    })
   }, [])
 
   const addNote = (event) => {
     event.preventDefault()
-    console.log('button clicked', event.target)
     const noteObject = {
       content: newNote,
       important: Math.random() > 0.5,
-      id: String(notes.length + 1),
     }
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    axios.post('http://localhost:3001/notes', noteObject).then((response) => {
+      setNotes(notes.concat(response.data))
+      setNewNote('')
+    })
   }
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
     setNewNote(event.target.value)
   }
 
-  const notesToShow = showAll? notes: notes.filter(note => note.important)
-  
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important)
+
   return (
     <div>
       <h1>Notes</h1>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
+          show {showAll ? 'important' : 'all'}
         </button>
-      </div>  
+      </div>
       <ul>
-        {notesToShow.map(note =>
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
-        )}
+        ))}
       </ul>
       <form onSubmit={addNote}>
         <input value={newNote} onChange={handleNoteChange} />
@@ -61,4 +53,4 @@ const App = (props) => {
   )
 }
 
-export default App 
+export default App
